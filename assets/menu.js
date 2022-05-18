31a4ab5db4b57feb7e3abb7aba3553365a3c2086
Jakub Strzępka <99281424+Kubastrzt@users.noreply.github.com>
2022-05-18 21:04:06 +0200

    Cleaning code

--- assets/menu.js
+++ assets/menu.js
@@ -1,4 +1,3 @@
-
 const linksToHover=document.querySelectorAll('.links')
 
 for(var i=0;i<linksToHover.length; i++)
@@ -9,6 +8,7 @@ for(var i=0;i<linksToHover.length; i++)
         var linkFocusedDropdown= linkFocus.children[1]
         linkFocusedDropdown.classList.toggle('toggle-dropdown')
     })
+    
     link.addEventListener('mouseleave', (e)=>{
         var linkFocus = e.target
         var linkFocusedDropdown= linkFocus.children[1]
